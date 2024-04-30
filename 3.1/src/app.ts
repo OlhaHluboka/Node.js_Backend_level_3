6d38ec069098592dd3c39335f33663a370187cd6
OlhaHluboka <voluntas007@gmail.com>
2024-04-30 22:43:06 +0300

    task1+

---
+++ 3.1/src/app.ts
@@ -0,0 +1,8 @@
+type DialogButtonType = "Yes" | "No";
+
+interface FormButton {
+    type: "Add" | "Remove" | "Buy"
+}
+
+// задача 1: создайте тип AnyButtonType, который описывает все вариации кнопок
+type AnyButtonType = DialogButtonType | FormButton["type"]; 
